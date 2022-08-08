8da174f89d278aa4d3928049d675638021185f2d
Alperen Berk IŞILDAR <alperenberkisildar@gmail.com>
2022-08-08 20:01:11 +0300

    Update

--- backend/src/main/java/amr22/linkedinhuceng/backend/controller/event/AnnouncementController.java
+++ backend/src/main/java/amr22/linkedinhuceng/backend/controller/event/AnnouncementController.java
@@ -6,7 +6,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.Optional; 
 
 @RestController
 @RequestMapping("/api/v1/announcements")
